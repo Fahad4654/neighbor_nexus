9c83ae35e5a7e613110848d100bbeb0b23a180aa
fahad4654 <kabirkaife@gmail.com>
2025-12-08 19:07:13 +0600

    types added

--- src/types/tool.ts
+++ src/types/tool.ts
@@ -6,3 +6,18 @@ export interface ToolWithDistance extends Tool {
   durationSeconds: number;
   durationText: string;
 }
+
+export type ToolSortableFields =
+  | "listing_type"
+  | "title"
+  | "description"
+  | "hourly_price"
+  | "daily_price"
+  | "security_deposit"
+  | "is_available"
+  | "distance";
+
+export interface SortOption {
+  column: ToolSortableFields;
+  order: "ASC" | "DESC";
+}
