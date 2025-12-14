8b3fb9b977a9f3584afffcce06f4cb2283371e84
fahad4654 <kabirkaife@gmail.com>
2025-12-14 18:09:30 +0600

    FEAT: get rent request by lender id controller is added

--- src/controllers/rentRequest/get.rentRequest.controller.ts
+++ src/controllers/rentRequest/get.rentRequest.controller.ts
@@ -117,3 +117,62 @@ export async function getRentRequestByBorrowerIdController(
     return handleUncaughtError(res, error, "Error fetching Rent Requests");
   }
 }
+
+export async function getRentRequestByLenderIdController(
+  req: Request,
+  res: Response
+) {
+  try {
+    if (!req.body) {
+      console.log("Request body is required for filtering/pagination");
+      return errorResponse(
+        res,
+        "Request body is required",
+        "Empty request body for required parameters",
+        400
+      );
+    }
+    const { lender_id, order, asc, page = 1, pageSize = 10 } = req.body;
+
+    const reqBodyValidation = validateRequiredBody(req, res, [
+      "lender_id",
+      "order",
+      "asc",
+    ]);
+    if (!reqBodyValidation) return;
+
+    if (!req.user) {
+      return errorResponse(res, "User is required", "Login is required", 401);
+    }
+
+    if (req.user.id !== lender_id && !req.user.isAdmin) {
+      return errorResponse(
+        res,
+        "Forbidden",
+        "You are not authorized to view this Rent Request",
+        403
+      );
+    }
+
+    const [rentRequestsResult] = await findByBorrowerId(
+      lender_id,
+      order,
+      asc,
+      Number(page),
+      Number(pageSize)
+    );
+    const { total, ...restOfPagination } = rentRequestsResult.pagination;
+    const pagination = { totalCount: total, ...restOfPagination };
+
+    return successResponse(
+      res,
+      "Rent Requests fetched successfully",
+      rentRequestsResult.data,
+      200,
+      pagination
+    );
+  } catch (error) {
+    console.error("Error fetching Rent Requests:", error);
+    return handleUncaughtError(res, error, "Error fetching Rent Requests");
+  }
+}
