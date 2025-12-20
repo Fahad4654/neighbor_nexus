d5c32bc29e3c06377fc8739af2bfe5f66f5fcd63
fahad4654 <kabirkaife@gmail.com>
2025-12-21 03:21:31 +0600

    FEAT: get transaction by flags

--- src/services/transacion/find.transacion.service.ts
+++ src/services/transacion/find.transacion.service.ts
@@ -1,27 +1,27 @@
 import { Transaction } from "../../models/Transaction";
 
-export async function findTransactionsByUserId(
-  user_id: string,
+export async function findTransactionsByBorrowerId(
+  borrower_id: string,
   page: number,
   pageSize: number
 ) {
   const transactions = await Transaction.findAll({
-    where: { reviewer_id: user_id },
+    where: { borrower_id, show_to_borrower: true },
     offset: (page - 1) * pageSize,
     limit: pageSize,
   });
   return transactions;
 }
 
-export async function findTransactionsByReviewerId(
-  reviewer_id: string,
+export async function findTransactionsByLenderId(
+  lender_id: string,
   page: number = 1,
   pageSize: number = 10
 ) {
   const offset = (page - 1) * pageSize;
 
   const transactions = await Transaction.findAll({
-    where: { reviewer_id },
+    where: { lender_id, show_to_lender: true },
     offset: offset,
     limit: pageSize,
   });
@@ -36,12 +36,21 @@ export async function findTransactionsByTransactionId(transaction_id: string) {
 
 export async function findTransactionsByListingId(listing_id: string) {
   const transactions = await Transaction.findAll({
-    where: { listing_id },
+    where: { listing_id, show_to_lender: true },
   });
   return transactions;
 }
 
-export async function findTransactionsByRentRequestId(rent_request_id: string) {
+export async function findTransactionsByRentRequestId(
+  rent_request_id: string,
+  lender: boolean
+) {
+  let whereClause: any = {};
+  if (lender) {
+    whereClause = { rent_request_id, show_to_lender: true };
+  } else {
+    whereClause = { rent_request_id, show_to_borrower: true };
+  }
   const transactions = await Transaction.findOne({
     where: { rent_request_id },
   });
